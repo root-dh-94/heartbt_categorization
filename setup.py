799fd7b6ca07cb66f88e3a426f9eb2c3e2afb432
sda126 <sda126@sfu.ca>
2023-05-25 18:01:49 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Heartbeat classification based on ECG data, to detect arrythmias and myocardial infarction',
+    author='Sushrut Dhaka',
+    license='',
+)
