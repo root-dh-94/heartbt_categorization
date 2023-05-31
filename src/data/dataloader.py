d98807f6afd8ece890c9a180cc138a34e9e163ec
sda126 <sda126@sfu.ca>
2023-05-30 21:50:02 -0700

    implemented augmentation by adding gaussian noise to training data

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,2 +1,25 @@
 import pandas as pd
 import torch.utils.data as data
+from sklearn.model_selection import train_test_split
+import numpy as np
+import torch
+
+class heartBeatDataset(data.Dataset):
+    def __init__(self, csv_path, train = True, seed = 42):
+        self.data = pd.read_csv(csv_path, header=None)
+        self.train = train
+        self.seed = seed
+
+    def __len__(self):
+        return len(self.data)
+
+
+    def __getitem__(self, index):
+        value = np.asarray(self.data.iloc[index, :-1])
+        label = self.data.iloc[index, -1]
+
+        if self.train:
+            value += np.random.normal(0, 0.1, value.shape)
+
+        return torch.from_numpy(value), label
+    
