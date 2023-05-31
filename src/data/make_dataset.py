38d282b91c6a37fafb45ba1d46a79bdaec2fd73c
sda126 <sda126@sfu.ca>
2023-05-30 17:12:50 -0700

    implementing data split into train/val set

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,10 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import torch.utils.data as data
+from sklearn.model_selection import train_test_split
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+def split_train_val(path):
+    data = pd.read_csv(path, header=None)
+    train_data, val_data = train_test_split(data, test_size=0.3, random_state=42)
+    train_data.to_csv('../../data/interim/train.csv', index=False)
+    val_data.to_csv('../../data/processed/val.csv', index=False)
+    return train_data, val_data
