8c7203a031204eec9faecd133a05822638530554
sda126 <sda126@sfu.ca>
2023-05-31 22:33:47 -0700

    corrected error with loading interim training dataset by removing header=None

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -23,12 +23,12 @@ def preprocess_data(path, train, seed=42):
 
 def train_resample(train_data_path, samples_class=10000, seed=42):
     #upsample minority class to balance training dataset
-    train_data = pd.read_csv(train_data_path, header=None)
-    train_data_majority = train_data[train_data.label==0]
-    train_data_minority_S = train_data[train_data.label==1]
-    train_data_minority_V = train_data[train_data.label==2]
-    train_data_minority_F = train_data[train_data.label==3]
-    train_data_minority_Q = train_data[train_data.label==4]
+    train_data = pd.read_csv(train_data_path)
+    train_data_majority = train_data[train_data.labels==0]
+    train_data_minority_S = train_data[train_data.labels==1]
+    train_data_minority_V = train_data[train_data.labels==2]
+    train_data_minority_F = train_data[train_data.labels==3]
+    train_data_minority_Q = train_data[train_data.labels==4]
 
     train_data_majority = train_data_majority.sample(n=samples_class, random_state=seed)
     train_data_minority_upsampled_S = resample(train_data_minority_S, replace=True, n_samples=samples_class, random_state=seed)
@@ -41,6 +41,10 @@ def train_resample(train_data_path, samples_class=10000, seed=42):
                                       train_data_minority_upsampled_Q])
     
     train_data_upsampled.to_csv('../../data/processed/train_upsampled.csv', index=False)
-    
-    print('Train data resampling completed.')
+    print('Training data resampling completed.')
     return
+
+if __name__ == '__main__':
+    preprocess_data('../../data/raw/heartbt_data/mitbih_train.csv', train=True)
+    preprocess_data('../../data/raw/heartbt_data/mitbih_test.csv', train=False)
+    train_resample('../../data/interim/train.csv')
