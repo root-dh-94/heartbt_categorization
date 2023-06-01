375e2e5b45d28083cd426bad2500692b71f3afbf
sda126 <sda126@sfu.ca>
2023-05-31 22:32:20 -0700

    corrected architecure error - included 5 residual blocks instead of 1

--- models/model.py
+++ models/model.py
@@ -4,39 +4,66 @@ import torch.nn.functional as F
 
 
 class Classifier(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size = 256):
+    def __init__(self, output_size, hidden_size = 64):
         super().__init__()
-        self.input_size = input_size
         self.output_size = output_size
         self.hidden_size = hidden_size
 
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=5, stride=1)
-        self.pre_residual_layer = nn.Sequential(nn.Conv1d(in_channels=32, out_channels=32, kernel_size=5, 
-                                                          stride=1), nn.ReLU(), nn.Conv1d(in_channels=32, 
-                                                          out_channels=32, kernel_size=5, stride=1))
+        self.conv1 = nn.Sequential(nn.Conv1d(in_channels=1, out_channels=32, kernel_size=5, stride=1))
+        self.pre_residual_layer_1 = nn.Sequential(nn.Conv1d(in_channels=32, out_channels=32, kernel_size=5, 
+                                                          stride=1, padding = 2), nn.ReLU(), 
+                                                          nn.Conv1d(in_channels=32, 
+                                                          out_channels=32, kernel_size=5, stride=1, padding = 2))
         
-        self.post_residual_layer = nn.Sequential(nn.ReLU(), nn.MaxPool1d(kernel_size=5, stride=2, padding=2))
+        self.post_residual_layer_1 = nn.Sequential(nn.ReLU(), nn.MaxPool1d(kernel_size=5, stride=2))
+
+        self.pre_residual_layer_2 = nn.Sequential(nn.Conv1d(in_channels=32, out_channels=32, kernel_size=5, 
+                                                          stride=1, padding = 2), nn.ReLU(), 
+                                                          nn.Conv1d(in_channels=32, 
+                                                          out_channels=32, kernel_size=5, stride=1, padding = 2))
+        
+        self.post_residual_layer_2 = nn.Sequential(nn.ReLU(), nn.MaxPool1d(kernel_size=5, stride=2))
+
+        self.pre_residual_layer_3 = nn.Sequential(nn.Conv1d(in_channels=32, out_channels=32, kernel_size=5, 
+                                                          stride=1, padding = 2), nn.ReLU(), 
+                                                          nn.Conv1d(in_channels=32, 
+                                                          out_channels=32, kernel_size=5, stride=1, padding = 2))
+        
+        self.post_residual_layer_3 = nn.Sequential(nn.ReLU(), nn.MaxPool1d(kernel_size=5, stride=2))
+
+        self.pre_residual_layer_4 = nn.Sequential(nn.Conv1d(in_channels=32, out_channels=32, kernel_size=5, 
+                                                          stride=1, padding = 2), nn.ReLU(), 
+                                                          nn.Conv1d(in_channels=32, 
+                                                          out_channels=32, kernel_size=5, stride=1, padding = 2))
+        
+        self.post_residual_layer_4 = nn.Sequential(nn.ReLU(), nn.MaxPool1d(kernel_size=5, stride=2))
+        
+        self.pre_residual_layer_5 = nn.Sequential(nn.Conv1d(in_channels=32, out_channels=32, kernel_size=5, 
+                                                          stride=1, padding = 2), nn.ReLU(), 
+                                                          nn.Conv1d(in_channels=32, 
+                                                          out_channels=32, kernel_size=5, stride=1, padding = 2))
+        
+        self.post_residual_layer_5 = nn.Sequential(nn.ReLU(), nn.MaxPool1d(kernel_size=5, stride=2))
         self.fc1 = nn.Sequential(nn.Linear(self.hidden_size, 32), nn.ReLU())
         self.fc2 = nn.Linear(32, self.output_size)
-
 
     def forward(self, x):
         conv_layer = self.conv1(x)
 
-        residual_block1_part1 = self.pre_residual_layer(conv_layer)
-        residual_block1_part2 = self.post_residual_layer(conv_layer + residual_block1_part1)
+        residual_block1_part1 = self.pre_residual_layer_1(conv_layer)
+        residual_block1_part2 = self.post_residual_layer_1(conv_layer + residual_block1_part1)
 
-        residual_block2_part1 = self.pre_residual_layer(residual_block1_part2)
-        residual_block2_part2 = self.post_residual_layer(residual_block1_part2 + residual_block2_part1)
+        residual_block2_part1 = self.pre_residual_layer_2(residual_block1_part2)
+        residual_block2_part2 = self.post_residual_layer_2(residual_block1_part2 + residual_block2_part1)
 
-        residual_block3_part1 = self.pre_residual_layer(residual_block2_part2)
-        residual_block3_part2 = self.post_residual_layer(residual_block2_part2 + residual_block3_part1)
+        residual_block3_part1 = self.pre_residual_layer_3(residual_block2_part2)
+        residual_block3_part2 = self.post_residual_layer_3(residual_block2_part2 + residual_block3_part1)
 
-        residual_block4_part1 = self.pre_residual_layer(residual_block3_part2)
-        residual_block4_part2 = self.post_residual_layer(residual_block3_part2 + residual_block4_part1)
+        residual_block4_part1 = self.pre_residual_layer_4(residual_block3_part2)
+        residual_block4_part2 = self.post_residual_layer_4(residual_block3_part2 + residual_block4_part1)
 
-        residual_block5_part1 = self.pre_residual_layer(residual_block4_part2)
-        residual_block5_part2 = self.post_residual_layer(residual_block4_part2 + residual_block5_part1)
+        residual_block5_part1 = self.pre_residual_layer_5(residual_block4_part2)
+        residual_block5_part2 = self.post_residual_layer_5(residual_block4_part2 + residual_block5_part1)
 
         fc1_input = nn.Flatten()(residual_block5_part2)
         fc1_output = self.fc1(fc1_input)
