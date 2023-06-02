7efc222abd26b6b409387fc6e9f2945fd21ddab9
sda126 <sda126@sfu.ca>
2023-06-02 12:45:29 -0700

    final training loop implementation

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -20,6 +20,7 @@ sys.path.append('/Users/sushrutdhaka/ecg_heartbt_categorization/src/data')
 from dataloader import heartBeatDataset
 sys.path.append('/Users/sushrutdhaka/ecg_heartbt_categorization/models')
 from model import Classifier
+import wandb
 
 
 class heartBeatClass(pl.LightningModule):
@@ -28,6 +29,13 @@ class heartBeatClass(pl.LightningModule):
         
         self.model = model
 
+        # Hyperparameters
+        self.lr = lr
+        self.batch_size = batch_size
+        self.seed = seed
+        self.betas = betas
+        self.num_classes = num_classes
+
         # Metrics
         self.train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=self.num_classes)
         self.valid_acc = torchmetrics.Accuracy(task="multiclass", num_classes=self.num_classes)
@@ -35,13 +43,7 @@ class heartBeatClass(pl.LightningModule):
         self.valid_confusion_matrix = torchmetrics.ConfusionMatrix(task='multiclass',num_classes=self.num_classes)
         self.test_confusion_matrix = torchmetrics.ConfusionMatrix(task='multiclass',num_classes=self.num_classes)
 
-        # Hyperparameters
-        self.lr = lr
-        self.batch_size = batch_size
-        self.seed = seed
-        self.betas = betas
-        self.num_classes = num_classes
-        self.save_hyperparameters()
+        self.validation_step_outputs = []
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, betas=self.betas)
@@ -59,11 +61,8 @@ class heartBeatClass(pl.LightningModule):
         # Calculate and log metrics
         pred = nn.Softmax(dim=1)(pred)
         pred = torch.argmax(pred, dim=1)
-        self.train_acc(pred, y)
         self.log('train_loss', loss, on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
-        self.log('train_acc', self.train_acc, on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
-        self.log('hyperparam_lr', self.optimizers().param_groups[0]['lr'], on_epoch=True, sync_dist=True, 
-                 prog_bar=True, logger=True)
+        self.log('train_acc', self.train_acc(pred,y), on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
 
         return loss
     
@@ -79,30 +78,29 @@ class heartBeatClass(pl.LightningModule):
         # Calculate and log metrics
         pred = nn.Softmax(dim=1)(pred)
         pred = torch.argmax(pred, dim=1)
-        self.valid_acc(pred, y)
+        #preds = [p.item() for p in pred]
+        #y_hat = [p.item() for p in y]
         self.log('val_loss', loss, on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
-        self.log('val_acc', self.valid_acc, on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
-        #self.log('val_confusion_matrix', self.valid_confusion_matrix(pred, y), on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
+        self.log('val_acc', self.valid_acc(pred,y), on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
+        #self.log('val_confusion_matrix', self.valid_confusion_matrix(preds, y_hat), on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
 
-    def test_step(self, test_batch, batch_idx):
+
+    def predict_step(self, test_batch, batch_idx):
         x, y = test_batch
         x = x.view(-1,1,187)
         pred = self.model(x)
 
-        # Loss
-        criterion = nn.CrossEntropyLoss()
-        loss = criterion(pred, y)
-
         # Calculate and log metrics
         pred = nn.Softmax(dim=1)(pred)
         pred = torch.argmax(pred, dim=1)
-        self.valid_acc(pred, y)
-        self.log('test_loss', loss, on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
-        self.log('test_acc', self.valid_acc, on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
-        #self.log('test_confusion_matrix', self.valid_confusion_matrix(pred, y), on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
+        #self.log('test_loss', loss, on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
+        #self.log('test_acc', self.test_acc(pred,y), on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
+        #self.log('test_confusion_matrix', self.test_confusion_matrix(pred, y), on_epoch=True, sync_dist=True, prog_bar=True, logger=True)
+        return pred,y
 
 
 if __name__ == '__main__':
+    print('Training model...')
     args = argparse.ArgumentParser()
     args.add_argument('--train_path', type=str, default='../../data/processed/train_upsampled.csv')
     args.add_argument('--val_path', type=str, default='../../data/processed/val.csv')
@@ -110,9 +108,9 @@ if __name__ == '__main__':
     args.add_argument('--lr', type=float, default=1e-3)
     args.add_argument('--betas', type=tuple, default=(0.9,0.999))
     args.add_argument('--num_classes', type=int, default=5)
-    args.add_argument('--max_epochs', type=int, default=100)
+    args.add_argument('--max_epochs', type=int, default=50)
     args.add_argument('--seed', type=int, default=42)
-    args.add_argument('ckpts', type=str, default='ckpts/', help='path to save checkpoints')
+    args.add_argument('--checkpoint_folder', type=str, default='ckpts/', help='path to save checkpoints')
     args = args.parse_args()
 
     pl.seed_everything(args.seed, workers=True)
@@ -136,13 +134,11 @@ if __name__ == '__main__':
 
     # training
     wandb_logger = WandbLogger()
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(monitor='val_loss', save_top_k=5, mode='min', dirpath=args.ckpts,
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(monitor='val_loss', save_top_k=5, mode='min', 
+                                                       dirpath=f"ckpts/lr{args.lr}-batch{args.batch_size}/",
                                                        filename='model-{epoch:02d}-{val_loss:.2f}')
-    trainer = pl.Trainer(devices='auto', accelerator='cpu', max_epochs=args.max_epochs, logger=wandb_logger, 
+    trainer = pl.Trainer(devices='auto', accelerator='gpu', max_epochs=args.max_epochs, logger=wandb_logger,
             log_every_n_steps=1, deterministic=True, callbacks=[pl.callbacks.early_stopping.EarlyStopping
-            (monitor='val_loss', patience=10, mode='min'), checkpoint_callback],default_root_dir=args.ckpts)
+            (monitor='val_loss', patience=10, mode='min'), checkpoint_callback],default_root_dir=args.checkpoint_folder)
     
     trainer.fit(model, train_loader, val_loader)
-
-    
-    
